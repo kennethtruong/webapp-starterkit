50ada7f9bee4dd7ff9abffaa3753cbca0183b144
Kenneth Truong <kenneth.e.truong@gmail.com>
2016-03-04 13:14:08 -0800

    refactor(mLab) update secret.js for mLab

--- api/config/secrets.js
+++ api/config/secrets.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: process.env.MONGODB || 'mongodb://localhost/react-dev-webkit',
+  db: process.env.MONGOLAB_URI || 'mongodb://localhost/react-dev-webkit',
 
   session: process.env.SESSION_SECRET || 'Your session secret goes here'
 };
